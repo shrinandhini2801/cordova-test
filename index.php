59c9674b97460c58a17be5f434f652cb5dea9b36
Rajib Paudyal <rajib.mp@gmail.com>
2014-09-28 13:46:29 +0300

    application and folder restructure

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
-// Load the project via public folder
-require 'public/index.php';
+error_reporting(E_ALL);
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$twig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . '/public/templates'));
+$client = new Transfluent\BackendClient\BackendClient();
+$lang = new Transfluent\MobileApp\model\Language($client);
+
+$title = new Transfluent\MobileApp\lib\Title();
+$header = new Transfluent\MobileApp\lib\PageHeader();
+
+###########################################################################################
+# For Development purpose debug mode is true and should be changed in Production to false #
+###########################################################################################
+$app['debug'] = true;
+
+require __DIR__ . '/app/Routes.php';
+
+$app->run();
--- public/index.php
+++ public/index.php
@@ -1,22 +1,4 @@
 <?php
 
-error_reporting(E_ALL);
-
-require __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
-
-$app = new Silex\Application();
-$twig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . '../app/templates'));
-$client = new Transfluent\BackendClient\BackendClient();
-$lang = new Transfluent\MobileApp\model\Language($client);
-
-$title = new Transfluent\MobileApp\lib\Title();
-$header = new Transfluent\MobileApp\lib\PageHeader();
-
-###########################################################################################
-# For Development purpose debug mode is true and should be changed in Production to false #
-###########################################################################################
-$app['debug'] = true;
-
-require __DIR__ . DIRECTORY_SEPARATOR .  '../app/routes.php';
-
-$app->run();
+// Load the project via protected folder
+require __DIR__. '/../index.php';
